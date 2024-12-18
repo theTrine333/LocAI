24b37a34c3fb5d05cb9964b852ff9579c78cd6cc
Prime Tesla <trinekaka@gmail.com>
2024-12-18 23:07:36 +0300

    Updated the UI

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,43 +1,73 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
+import { HapticTab } from "@/components/HapticTab";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { height, width } from "@/constants/Styles";
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
+        tabBarHideOnKeyboard: true,
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
+            position: "absolute",
           },
-          default: {},
+          default: {
+            position: "absolute",
+            bottom: 20,
+            right: 20,
+            left: 20,
+            width: width * 0.9,
+            height: height * 0.07,
+            borderRadius: 8,
+            marginLeft: 20,
+          },
         }),
-      }}>
+      }}
+    >
       <Tabs.Screen
-        name="index"
+        name="history"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "History",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="history" color={color} size={28} />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="index"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Chat",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="keyboard-voice" color={color} size={28} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="cog-outline"
+              color={color}
+              size={28}
+            />
+          ),
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,101 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import {
+  Image,
+  StyleSheet,
+  Platform,
+  View,
+  TextInput,
+  ActivityIndicator,
+  FlatList,
+  RefreshControl,
+  TouchableOpacity,
+  useColorScheme,
+} from "react-native";
+import { height, Styles } from "@/constants/Styles";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { useState } from "react";
+import { sendMessageToBackend } from "@/api/scripts";
+import { chat } from "@/types";
+import Message from "@/components/Message";
+import { useNavigation } from "expo-router";
 
 export default function HomeScreen() {
+  const [started, setStarted] = useState(true);
+  const [prompt, setPrompt] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [refresh, setRefresh] = useState(false);
+  // const [chats, setChats] = useState<chat[]>([{}]);
+  const chats: chat[] = [
+    {
+      time: "21:13:46",
+      role: "system",
+      prompt: "Hello There",
+    },
+    {
+      time: "21:13:48",
+      role: "assistant",
+      prompt: "How, how may i help you ?",
+    },
+    {
+      time: "21:14:48",
+      role: "system",
+      prompt: "Not much actually, Can you tell me about yourself?",
+    },
+    {
+      time: "21:15:46",
+      role: "assistant",
+      prompt: "Ofcourse yes, I'm a locally runned large language modal(llm)",
+    },
+    {
+      time: "21:16:48",
+      role: "system",
+      prompt: "Oooh nice, so whats a language modal?",
+    },
+    {
+      time: "21:17:48",
+      role: "assistant",
+      prompt:
+        "It's just a subset of Artificial Intelligence that understands the human language",
+    },
+    {
+      time: "21:18:48",
+      role: "system",
+      prompt: "Oooh nice, so whats a language modal?",
+    },
+    {
+      time: "21:19:48",
+      role: "assistant",
+      prompt:
+        "It's just a subset of Artificial Intelligence that understands the human language",
+    },
+  ];
+  const theme = useColorScheme() ?? "light";
+  const navigation = useNavigation();
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <ThemedView style={[Styles.mainContainer, { justifyContent: "center" }]}>
+      <Image
+        source={require("@/assets/images/icon.png")}
+        style={[
+          Styles.modalImage,
+          { height: 120, width: 120, marginBottom: 10, alignSelf: "center" },
+        ]}
+        resizeMode="contain"
+      />
+      <ThemedText type="title">LOCAI</ThemedText>
+      <ThemedText
+        type="subtitle"
+        style={{ marginTop: 40, textAlign: "center", color: "grey" }}
+      >
+        Free local GPT's for everyone{"\n"}All this knowledge-base backedup
+      </ThemedText>
+      <TouchableOpacity
+        style={Styles.continueBtn}
+        onPress={() => {
+          navigation.navigate("chat");
+        }}
+      >
+        <ThemedText style={Styles.continueText}>Continue</ThemedText>
+      </TouchableOpacity>
+    </ThemedView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
